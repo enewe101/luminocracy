5e47cf0d6bda91f98e346ee7fa869b8bfa60ca00
Stefan Dimitrov <stefan@dimitrov.li>
2015-06-15 15:14:44 -0400

    Second Mock using Grunt + Bower + Yeoman .

---
+++ mock2/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name luminocracyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the luminocracyApp
+ */
+angular.module('luminocracyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
