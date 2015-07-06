62796a85618067f8b19ec83fc12f746c89ccd08e
Stefan Dimitrov <elwebmaster@users.noreply.github.com>
2015-07-06 16:41:25 +0000

    Switched to SQLite

--- mock1/src/lumi_rest/lumi_rest/settings.py
+++ mock1/src/lumi_rest/lumi_rest/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'lumi_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': 'luminocracy',
 		'USER': 'luminocracy',
 		'PASSWORD':'devpass',
